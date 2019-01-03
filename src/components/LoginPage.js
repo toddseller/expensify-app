392f368f65c2dc18928115b44040c72afdbf5dd9
Todd Seller <me@toddseller.com>
2019-01-03 06:42:35 -0800

    Styling for login page and button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,15 @@ import { connect } from 'react-redux'
 import { startLogin } from '../actions/auth'
 
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <button onClick={ startLogin }>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify App</h1>
+      <p>It's time to get your expenses under control.</p>
+      <div className="login-button">
+
+        <button className="login-button__button" onClick={ startLogin }><img src="/images/btn_google_light_normal_ios.svg" className="login-button__image"/><span className="login-button__text">Login with Google</span></button>
+      </div>
+    </div>
   </div>
 )
 
